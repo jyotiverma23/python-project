82c59431e62a65cfaf467a9904514ff02c8ea632
jyotiverma23 <103823481+jyotiverma23@users.noreply.github.com>
2022-05-20 12:16:28 +0530

    Update main.py
    bank of baroda password

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ a=int(input("value of the number a"))
 b=int(input("value of the number b"))
 c=a+b
 print(c) 
+
+p=input("enter your passwerd")
+if p=="123" or p=="cs2018" or p=="jyoti":
+  print("welcome to bank of baroda your")
+else:
+  print("invalid password")
